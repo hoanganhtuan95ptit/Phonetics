8e934edf90b75c0d8c1d0baf7ec387876daf86fd
Hoang Anh Tuan <hoanganhtuan95ptit@gmail.com>
2023-09-20 22:08:28 +0700

    add  ads

--- app/src/main/java/com/simple/phonetics/ui/MainActivity.kt
+++ app/src/main/java/com/simple/phonetics/ui/MainActivity.kt
@@ -4,11 +4,19 @@ import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
+import androidx.core.view.updatePadding
 import androidx.lifecycle.lifecycleScope
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.MobileAds
+import com.simple.analytics.logAnalytics
 import com.simple.bottomsheet.ActivityScreen
 import com.simple.coreapp.ui.base.activities.BaseViewBindingActivity
+import com.simple.coreapp.utils.extentions.doOnHeightNavigationChange
 import com.simple.coreapp.utils.extentions.getColorFromAttr
 import com.simple.coreapp.utils.extentions.toPx
+import com.simple.crashlytics.logCrashlytics
 import com.simple.phonetics.databinding.ActivityMainBinding
 import com.simple.phonetics.ui.phonetics.PhoneticsFragment
 
@@ -32,42 +40,40 @@ class MainActivity : BaseViewBindingActivity<ActivityMainBinding>(), ActivityScr
 
         val binding = binding ?: return
 
-//        MobileAds.initialize(this) { status ->
-//            logAnalytics("MobileAds" to "MobileAds", *status.adapterStatusMap.map { Pair(it.key, it.value.initializationState.name) }.toTypedArray(), *status.adapterStatusMap.map { Pair(it.key, it.value.description) }.toTypedArray())
-//        }
-//
-//        binding.adView.loadAd(AdRequest.Builder().build())
-//        binding.adView.adListener = object : AdListener() {
-//
-//            override fun onAdClicked() {
-//                logAnalytics("AdClicked" to "AdClicked")
-//            }
-//
-//            override fun onAdOpened() {
-//                logAnalytics("AdOpened" to "AdOpened")
-//            }
-//
-//            override fun onAdImpression() {
-//                logAnalytics("AdImpression" to "AdImpression")
-//            }
-//
-//            override fun onAdFailedToLoad(p0: LoadAdError) {
-//                logCrashlytics(RuntimeException("${p0.code}-${p0.message}"))
-//            }
-//        }
-//
-//        doOnHeightNavigationChange {
-//
-//            binding.root.children.first().updatePadding(bottom = it)
-//        }
+        MobileAds.initialize(this) { status ->
+            logAnalytics("MobileAds" to "MobileAds", *status.adapterStatusMap.map { Pair(it.key, it.value.initializationState.name) }.toTypedArray(), *status.adapterStatusMap.map { Pair(it.key, it.value.description) }.toTypedArray())
+        }
 
-        (binding.root.parent as? View)?.setBackgroundColor(binding.root.context.getColorFromAttr(com.google.android.material.R.attr.colorOnBackground))
+        binding.adView.loadAd(AdRequest.Builder().build())
+        binding.adView.adListener = object : AdListener() {
+
+            override fun onAdClicked() {
+                logAnalytics("AdClicked" to "AdClicked")
+            }
+
+            override fun onAdOpened() {
+                logAnalytics("AdOpened" to "AdOpened")
+            }
+
+            override fun onAdImpression() {
+                logAnalytics("AdImpression" to "AdImpression")
+            }
+
+            override fun onAdFailedToLoad(p0: LoadAdError) {
+                logCrashlytics(RuntimeException("${p0.code}-${p0.message}"))
+            }
+        }
+
+        doOnHeightNavigationChange {
+
+            binding.root.updatePadding(bottom = it)
+        }
     }
 
     override fun onPercent(percent: Float) {
 
         val binding = binding ?: return
 
-        binding.root.setRadius(percent * 40.toPx())
+        binding.fragmentContainer.setRadius(percent * 40.toPx())
     }
 }
