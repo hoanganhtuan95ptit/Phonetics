b0158a8ce268deb62db4efc1e952ed419761543f
Hoang Anh Tuan <hoanganhtuan95ptit@gmail.com>
2024-11-07 12:06:27 +0700

    optimize

--- app/src/main/java/com/simple/phonetics/domain/entities/Language.kt
+++
@@ -1,5 +0,0 @@
-package com.simple.phonetics.domain.entities
-
-data class Language(val code: String, val name: String, val listIpa: List<Ipa>)
-
-data class Ipa(val code: String, val source: String)
