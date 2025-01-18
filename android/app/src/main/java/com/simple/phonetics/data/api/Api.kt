9c6416cf9e042c2ea4dcf9a6ff49facf36c36851
Hoang Anh Tuan <hoanganhtuan95ptit@gmail.com>
2025-01-18 18:11:42 +0700

    fix flag

--- android/app/src/main/java/com/simple/phonetics/data/api/Api.kt
+++ android/app/src/main/java/com/simple/phonetics/data/api/Api.kt
@@ -9,10 +9,10 @@ import retrofit2.http.Path
 import retrofit2.http.Streaming
 import retrofit2.http.Url
 
-interface Api {
+private val BRANCH: String
+    get() = if (BuildConfig.DEBUG) "develop" else "main"
 
-    private val BRANCH: String
-        get() = if (BuildConfig.DEBUG) "develop" else "main"
+interface Api {
 
     @GET("https://raw.githubusercontent.com/hoanganhtuan95ptit/Phonetics/refs/heads/{branch}/configs/translate/{language_code}/translates.json")
     suspend fun syncTranslate(@Path("language_code") languageCode: String, @Path("branch") branch: String = BRANCH): Map<String, String>
@@ -23,6 +23,4 @@ interface Api {
     @Streaming
     @GET
     suspend fun syncPhonetics(@Url url: String): ResponseBody
-
-
 }
