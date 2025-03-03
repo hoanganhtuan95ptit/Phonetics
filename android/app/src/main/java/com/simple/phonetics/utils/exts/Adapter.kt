56fcf92cbed03619fc00d78076a0310c8ce83fdb
Hoang Anh Tuan <hoanganhtuan95ptit@gmail.com>
2025-03-03 16:36:12 +0700

    optimize

--- android/app/src/main/java/com/simple/phonetics/utils/exts/Adapter.kt
+++ android/app/src/main/java/com/simple/phonetics/utils/exts/Adapter.kt
@@ -1,10 +1,12 @@
 package com.simple.phonetics.utils.exts
 
 import com.simple.adapter.ViewItemAdapter
+import com.simple.coreapp.ui.adapters.EmptyAdapter
 import com.simple.coreapp.ui.adapters.texts.NoneTextAdapter
 import com.tuanha.adapter.AdapterManager
 
 fun ListPreviewAdapter() = arrayOf(
+    EmptyAdapter(),
     NoneTextAdapter(),
     *AdapterManager.all().filterIsInstance<ViewItemAdapter<*, *>>().toTypedArray()
 )
