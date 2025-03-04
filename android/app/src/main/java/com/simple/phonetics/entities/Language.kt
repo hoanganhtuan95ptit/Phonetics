edce58ecef19d71900fd6e725d7d55aaa6aa937b
Hoang Anh Tuan <hoanganhtuan95ptit@gmail.com>
2025-03-04 14:23:10 +0700

    support language

--- android/app/src/main/java/com/simple/phonetics/entities/Language.kt
+++ android/app/src/main/java/com/simple/phonetics/entities/Language.kt
@@ -19,11 +19,29 @@ data class Language(
     )
 
     companion object {
-
+        
         const val EN = "en"
         const val VI = "vi"
         const val ZH = "zh"
-        const val KO = "ko"
+        const val ES = "es"
+        const val FR = "fr"
+        const val AR = "ar"
+        const val DE = "de"
+        const val EO = "eo"
+        const val FA = "fa"
+        const val FI = "fi"
+        const val IS = "is"
         const val JA = "ja"
+        const val KM = "km"
+        const val KO = "ko"
+        const val MA = "ma"
+        const val NB = "nb"
+        const val NL = "nl"
+        const val OR = "or"
+        const val RO = "ro"
+        const val SV = "sv"
+        const val SW = "sw"
+        const val TTS = "tts"
+        const val JAM = "jam"
     }
 }
