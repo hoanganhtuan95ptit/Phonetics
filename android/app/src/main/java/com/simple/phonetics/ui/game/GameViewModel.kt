8a312dbf8509abb4526d1ac3a50e05dd5c10c411
Hoang Anh Tuan <hoanganhtuan95ptit@gmail.com>
2025-03-14 15:47:06 +0700

    fix anim

--- android/app/src/main/java/com/simple/phonetics/ui/game/GameViewModel.kt
+++ android/app/src/main/java/com/simple/phonetics/ui/game/GameViewModel.kt
@@ -1,21 +1,37 @@
 package com.simple.phonetics.ui.game
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import com.simple.core.utils.extentions.orZero
 import com.simple.coreapp.utils.extentions.combineSources
-import com.simple.coreapp.utils.extentions.get
 import com.simple.coreapp.utils.extentions.getOrEmpty
 import com.simple.coreapp.utils.extentions.postDifferentValue
 import com.simple.phonetics.ui.base.fragments.BaseViewModel
 
 class GameViewModel : BaseViewModel() {
 
-    val title: LiveData<CharSequence> = combineSources(theme, translate) {
+    val title: LiveData<CharSequence> = combineSources(translate) {
 
-        val theme = theme.get()
         val translate = translate.getOrEmpty()
 
         val title = translate["game_screen_title"].orEmpty()
 
         postDifferentValue(title)
+    }
+
+    val consecutiveCorrectAnswers: LiveData<Int> = MediatorLiveData()
+
+
+    fun updateAnswerCorrect(isCorrect: Boolean) {
+
+        val count = if (isCorrect) {
+
+            consecutiveCorrectAnswers.value.orZero() + 1
+        } else {
+
+            0
+        }
+
+        consecutiveCorrectAnswers.postDifferentValue(count)
     }
 }
