8e145e560f8110078eb4c1b5083d24c11c1f1c64
Hoang Anh Tuan <hoanganhtuan95ptit@gmail.com>
2025-04-27 20:14:35 +0700

    1.2.61

--- android/app/src/main/java/com/simple/phonetics/di/ApiModule.kt
+++ android/app/src/main/java/com/simple/phonetics/di/ApiModule.kt
@@ -7,11 +7,42 @@ import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.jackson.JacksonConverterFactory
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
 import java.util.concurrent.TimeUnit
+import javax.net.ssl.SSLContext
+import javax.net.ssl.SSLSocketFactory
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 
 @JvmField
 val apiModule = module {
+
+    fun getUnsafeTrustManager(): X509TrustManager {
+
+        return object : X509TrustManager {
+            override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {
+                // Không kiểm tra client
+            }
+
+            override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {
+                // Không kiểm tra server
+            }
+
+            override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
+        }
+    }
+
+    fun getUnsafeSslSocketFactory(): SSLSocketFactory {
+
+        val trustAllCerts = arrayOf<TrustManager>(getUnsafeTrustManager())
+
+        val sslContext = SSLContext.getInstance("TLS")
+        sslContext.init(null, trustAllCerts, SecureRandom())
+
+        return sslContext.socketFactory
+    }
 
     single {
 
@@ -21,6 +52,7 @@ val apiModule = module {
         OkHttpClient
             .Builder()
             .connectionSpecs(listOf(ConnectionSpec.MODERN_TLS))
+            .sslSocketFactory(getUnsafeSslSocketFactory(), getUnsafeTrustManager())
             .readTimeout(20, TimeUnit.SECONDS)
             .writeTimeout(20, TimeUnit.SECONDS)
             .connectTimeout(20, TimeUnit.SECONDS)
