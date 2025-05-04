3789baf4308333655f12360d0d3ee4b67cc85ee4
Hoang Anh Tuan <hoanganhtuan95ptit@gmail.com>
2025-05-04 13:33:24 +0700

    1.2.63

--- android/app/src/main/java/com/simple/phonetics/ui/base/fragments/BaseSheetFragment.kt
+++ android/app/src/main/java/com/simple/phonetics/ui/base/fragments/BaseSheetFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.View
 import androidx.viewbinding.ViewBinding
 import com.simple.coreapp.ui.base.dialogs.sheet.BaseViewModelSheetFragment
+import com.simple.coreapp.utils.ext.setFullScreen
 
 abstract class BaseSheetFragment<T : ViewBinding, VM : BaseViewModel>() : BaseViewModelSheetFragment<T, VM>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        activity?.window?.setFullScreen()
+
         super.onViewCreated(view, savedInstanceState)
     }
 }
