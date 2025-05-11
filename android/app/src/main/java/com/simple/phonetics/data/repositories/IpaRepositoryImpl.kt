c4e389f29e123de13d38badb9e005919f304911d
Hoang Anh Tuan <hoanganhtuan95ptit@gmail.com>
2025-05-11 12:42:54 +0700

    optimize ipa

--- android/app/src/main/java/com/simple/phonetics/data/repositories/IpaRepositoryImpl.kt
+++ android/app/src/main/java/com/simple/phonetics/data/repositories/IpaRepositoryImpl.kt
@@ -1,18 +1,28 @@
 package com.simple.phonetics.data.repositories
 
-import com.simple.phonetics.data.api.Api
-import com.simple.dao.ipa.IpaDaoNew
-import com.simple.phonetics.domain.repositories.IpaRepository
 import com.simple.dao.entities.Ipa
+import com.simple.dao.ipa.IpaDaoNew
+import com.simple.phonetics.data.api.Api
+import com.simple.phonetics.data.dao.ipa.IpaDao
+import com.simple.phonetics.domain.repositories.IpaRepository
 import kotlinx.coroutines.flow.Flow
 
 class IpaRepositoryImpl(
     private val api: Api,
+    private val ipaDaoOld: IpaDao,
     private val ipaDao: IpaDaoNew
 ) : IpaRepository {
 
     override suspend fun syncIpa(languageCode: String): List<Ipa> {
         return api.syncIPA(languageCode = languageCode)
+    }
+
+    override suspend fun getAllOldAsync(languageCode: String): Flow<List<Ipa>> {
+        return ipaDaoOld.getListAsync(languageCode = languageCode)
+    }
+
+    override suspend fun countAlOld(languageCode: String): Int {
+        return ipaDaoOld.getCount(languageCode = languageCode)
     }
 
     override suspend fun getIpaAsync(languageCode: String): Flow<List<Ipa>> {
