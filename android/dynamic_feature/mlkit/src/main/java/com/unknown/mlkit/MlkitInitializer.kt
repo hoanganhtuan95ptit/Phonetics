608261f00ffc45935aa59ebd70f011a6c44c1078
Hoang Anh Tuan <hoanganhtuan95ptit@gmail.com>
2025-06-27 21:41:26 +0700

    fix bug dynamic feature

--- android/dynamic_feature/mlkit/src/main/java/com/unknown/mlkit/MlkitInitializer.kt
+++ android/dynamic_feature/mlkit/src/main/java/com/unknown/mlkit/MlkitInitializer.kt
@@ -1,23 +1,18 @@
 package com.unknown.mlkit
 
 import android.content.Context
-import androidx.startup.AppInitializer
 import androidx.startup.Initializer
-import com.google.mlkit.common.MlKit
-import com.simple.detect.mlkit.MlkitDetectInitializer
-import com.simple.translate.mlkit.MlkitTranslateInitializer
+
 
 class MlkitInitializer : Initializer<Unit> {
 
     override fun create(context: Context) {
 
-        MlKit.initialize(context)
-
-        AppInitializer.getInstance(context)
-            .initializeComponent(MlkitDetectInitializer::class.java)
-
-        AppInitializer.getInstance(context)
-            .initializeComponent(MlkitTranslateInitializer::class.java)
+//        AppInitializer.getInstance(context)
+//            .initializeComponent(MlkitDetectInitializer::class.java)
+//
+//        AppInitializer.getInstance(context)
+//            .initializeComponent(MlkitTranslateInitializer::class.java)
 
         return
     }
