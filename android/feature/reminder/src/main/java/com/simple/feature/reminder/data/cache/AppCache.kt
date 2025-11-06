d0934f04670dfa1b8da89787c03c0398d409eb3d
Hoang Anh Tuan <hoanganhtuan95ptit@gmail.com>
2025-11-06 20:52:12 +0700

    optimize reminder

--- android/feature/reminder/src/main/java/com/simple/feature/reminder/data/cache/AppCache.kt
+++ android/feature/reminder/src/main/java/com/simple/feature/reminder/data/cache/AppCache.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.core.content.edit
 import com.simple.phonetics.PhoneticsApp
+import java.util.Calendar
 
 internal object AppCache {
 
@@ -14,4 +15,9 @@ internal object AppCache {
     fun getTimeUserInteractInHome() = sharedPreferences.getLong("TimeUserInteractInHome", System.currentTimeMillis())
 
     fun updateTimeUserInteractInHome() = sharedPreferences.edit { putLong("TimeUserInteractInHome", System.currentTimeMillis()) }
+
+
+    fun getDateMorningReminder() = sharedPreferences.getInt("DateMorningReminder", 0)
+
+    fun updateDateMorningReminder() = sharedPreferences.edit { putInt("DateMorningReminder", Calendar.getInstance().get(Calendar.DAY_OF_YEAR)) }
 }
