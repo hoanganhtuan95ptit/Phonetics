01ff3b8e1f88a766c9f13fc548ccfb5d9603337f
Hoang Anh Tuan <hoanganhtuan95ptit@gmail.com>
2026-01-09 20:06:37 +0700

    1.5.23

--- android/app/src/main/java/com/simple/phonetics/ui/home/services/background/BackgroundHomeViewModel.kt
+++ android/app/src/main/java/com/simple/phonetics/ui/home/services/background/BackgroundHomeViewModel.kt
@@ -1,20 +1,12 @@
 package com.simple.phonetics.ui.home.services.background
 
-import com.simple.coreapp.ui.view.Size
 import com.simple.phonetics.ui.base.fragments.BaseViewModel
 import com.simple.phonetics.utils.exts.mutableSharedFlowWithDiff
 import com.simple.phonetics.utils.exts.value
 
 class BackgroundHomeViewModel : BaseViewModel() {
 
-    val backgroundSize = mutableSharedFlowWithDiff<Size>()
-
     val backgroundAlpha = mutableSharedFlowWithDiff<Float>()
-
-    fun updateBackgroundSize(size: Size) {
-        if (size == backgroundSize.value) return
-        backgroundSize.tryEmit(size)
-    }
 
     fun updateBackgroundAlpha(alpha: Float) {
         if (alpha == backgroundAlpha.value) return
